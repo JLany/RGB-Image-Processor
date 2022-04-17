24bc0fba431af4accb1cb26d926293327b7a6891
Kilany <y.m.elkilany@gmail.com>
2022-04-17 03:21:45 +0200

    Create invertFilter

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 #include <string>
 #include <cstring>
 #include <algorithm>
-#include "bmplib.h"
+#include "bmplib.cpp"
 
 using namespace std;
 
@@ -17,19 +18,26 @@ void invertFilter();        // 2
 
 int main() {
     char userInput;
-   printf("Welcome Habibi!\n");
-    // read image
+    printf("Welcome Habibi!\n");
+    readImage();
     while (true) {
         printf("Please Select a filter:\n1- Black & White Filter\n2- Invert Filter\n3- Merge Filter\n4 - Flip Image\n5 - Rotate Image\n");
         printf("6 - Darken and Lighten Image\n7 - Detect Image Edges\n8 - Enlarge Image\n9 - Shrink Image\na - Mirror Image\nb - Shuffle Image\nc - Blur Image\ns - Save changes\n0 - To end\n");
 
         cin >> userInput;
         switch (userInput) {
-            case '2': invertFilter();
+            case '2':
+                invertFilter();
+                printf("Image inverted.\n");
                 break;
-            case '0': printf("Good Bye!");
+            case 's':
+                writeImage();
+                printf("Save completed.\n");
+                break;
+            case '0':
+                printf("Good Bye!");
                 return 0;
-            default: printf("Invalid Input! Try Again.");
+            default: printf("Invalid Input! Try Again.\n");
         }
     }
 }
@@ -52,6 +60,14 @@ void writeImage() {
     writeRGBBMP(imageName, img);
 }
 
-void invertFilter() {
 
+
+void invertFilter() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            for (int k = 0; k < RGB; k++) {
+                img[i][j][k] = 255 - img[i][j][k];
+            }
+        }
+    }
 }
