0dcba70f6a08317f8147358f4c8ba7874775e31a
MayaZayn <mayozain5@gmail.com>
2022-04-19 21:35:25 +0200

    Added my 4 filters

---
+++ cmake-build-debug/CMakeFiles/RGB_Image_Processor.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Maya Ayman/Desktop/teamTask/RGB-Image-Processor/main.cpp" "CMakeFiles/RGB_Image_Processor.dir/main.cpp.obj" "gcc" "CMakeFiles/RGB_Image_Processor.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RGB_Image_Processor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RGB_Image_Processor.dir/main.cpp.obj"
+  "CMakeFiles/RGB_Image_Processor.dir/main.cpp.obj.d"
+  "RGB_Image_Processor.exe"
+  "RGB_Image_Processor.exe.manifest"
+  "RGB_Image_Processor.pdb"
+  "libRGB_Image_Processor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RGB_Image_Processor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
